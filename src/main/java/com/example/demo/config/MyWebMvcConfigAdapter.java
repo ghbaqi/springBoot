39bc305729b01ab5eca70258a2fb1be1a6081d80
gaohui <gaohui@hdsc.com>
2018-04-02 14:31:17 +0800

    webMvcConfigAdapter 的使用：1.增加自定义资源文件映射 2. 视图映射 3. 拦截器的使用

---
+++ src/main/java/com/example/demo/config/MyWebMvcConfigAdapter.java
@@ -0,0 +1,49 @@
+package com.example.demo.config;
+
+import com.example.demo.interceptor.LoginInterceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 对静态资源和拦截器进行功能扩展
+ * WebMvcConfigurerAdapter
+ */
+@Configuration
+public class MyWebMvcConfigAdapter extends WebMvcConfigurerAdapter {
+
+    /**
+     * 增加自定义的资源文件映射
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**").addResourceLocations("classpath:/image/");
+        super.addResourceHandlers(registry);
+    }
+
+    /**
+     * 为页面跳转添加映射
+     *
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/user/toLogin").setViewName("login");
+        super.addViewControllers(registry);
+    }
+
+    /**
+     * 添加拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/user/toLogin", "/user/login");
+        super.addInterceptors(registry);
+    }
+}
---
+++ src/main/java/com/example/demo/web/UserController.java
@@ -0,0 +1,32 @@
+package com.example.demo.web;
+
+import com.example.demo.entity.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+    @ResponseBody
+    @PostMapping("/login")
+    public Map<String, Object> login(HttpServletRequest request,String userName,String  password) {
+        HashMap<String, Object> map = new HashMap<>();
+        if ("root".equals(userName) && "root".equals(password)) {
+            map.put("result", "1");
+            User user = new User(userName, password);
+            request.getSession().setAttribute("user",user);
+        } else {
+            map.put("result", "0");
+        }
+        return map;
+    }
+}
